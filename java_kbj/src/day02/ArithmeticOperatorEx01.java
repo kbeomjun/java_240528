cb5c6f7695f0e8b7c1e4d420059a8f9d754b7f5f
kbeomjun <98301971+kbeomjun@users.noreply.github.com>
2024-05-29 16:18:26 +0900

    24.05.29 연산자

--- java_kbj/src/day02/ArithmeticOperatorEx01.java
+++ java_kbj/src/day02/ArithmeticOperatorEx01.java
@@ -32,6 +32,34 @@ public class ArithmeticOperatorEx01 {
 		System.out.println("str1 == str2 : "+(str1.equals(str2)));
 		System.out.println("str1 == str3 : "+(str1.equals(str3)));
 		System.out.println("str2 == str3 : "+(str1.equals(str3)));
+		
+		//논리연산자
+		int score = 85;
+		boolean isB = score >= 80 && score < 90;
+		System.out.println(isB);
+		
+		int sub1 = 60, sub2 = 100, sub3 = 30, sub4 = 80;
+		boolean isFail = sub1 < 40 || sub2<40 || sub3<40 || sub4<40;
+		System.out.println(isFail);
+		
+		int age = 10;
+		boolean isMinor = !(age >= 19);
+		System.out.println(isMinor);
+		
+		//복합 대입 연산자
+		int num3 = 5, num4 = 2; 
+		num3 += num4;
+		System.out.println(num3);
+		
+		//조건 연산자 : (조건식) ? 참일 때 값 : 거짓일 때 값
+		int num5 = 3;
+		String result = (num5%2 == 0) ? "짝수" : "홀수";
+		System.out.println(num5+"은(는) "+result+"입니다.");
+		
+		//비트연산자 : 변수 또는 값을 비트로 나열한 후 각 비트별로 논리 연산을 함. &(AND), |(OR), ~(NOT), ^(XOR)
+		
+		//비트 이동 연산자 <<, >>, >>>
+		
 	}
 
 }

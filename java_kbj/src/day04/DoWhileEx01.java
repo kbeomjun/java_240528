050ee85211dd7b76670468fb5885c2903708e4cb
kbeomjun <98301971+kbeomjun@users.noreply.github.com>
2024-05-31 17:12:11 +0900

    자바 4일차 - 조건문 dowhile

---
+++ java_kbj/src/day04/DoWhileEx01.java
@@ -0,0 +1,31 @@
+package day04;
+
+import java.util.Scanner;
+
+public class DoWhileEx01 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		/*
+		char ch = 'a';
+		do {
+			System.out.print("y 입력 : ");
+			ch = scan.next().charAt(0);
+		}while(ch != 'y');
+		System.out.println("y 입력 완료");
+		*/
+		int num = 0;
+		do {
+			System.out.println("메뉴");
+			System.out.println("1. 프로그램 실행");
+			System.out.println("2. 프로그램 저장");
+			System.out.println("3. 프로그램 불러오기");
+			System.out.println("4. 프로그램 종료");
+			System.out.print("메뉴 선택 : ");
+			num = scan.nextInt();
+		}while(num != 4);
+		System.out.println("프로그램을 종료합니다.");
+		
+	}
+
+}

e12592469cf78ea490fd7de68169caac9f6084d9
kbeomjun <98301971+kbeomjun@users.noreply.github.com>
2024-05-31 17:30:14 +0900

    자바 4일차 - 과제

--- java_kbj/src/day04/homework/UpDownGame.java
+++ java_kbj/src/day04/homework/UpDownGame.java
@@ -11,6 +11,7 @@ public class UpDownGame {
 		int min = 1, max = 100;
 		int random = (int)(Math.random() * (max - min + 1) + min);
 		System.out.println("랜덤한 수 : " + random);
+		
 		for( ; ; ) {
 			System.out.print("정수 입력 : ");
 			int num = scan.nextInt();

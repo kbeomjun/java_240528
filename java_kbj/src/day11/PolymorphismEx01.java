7ed067ad104167e51552b0316f519ded875201e2
kbeomjun <98301971+kbeomjun@users.noreply.github.com>
2024-06-13 12:34:59 +0900

    자바 9일차 - 다형성

--- java_kbj/src/day11/PolymorphismEx01.java
+++ java_kbj/src/day11/PolymorphismEx01.java
@@ -9,7 +9,7 @@ public class PolymorphismEx01 {
 		turnOn(aircon);
 	}
 	
-	// 매개변수의 다형성 : 메소드에 부모 객체를 사용함으로써 자식 객체를 호출
+	// 매개변수의 다형성 : 메서드에 부모 객체를 사용함으로써 자식 객체를 호출
 	public static void turnOn(Appliances app) {
 		app.power = true;
 		System.out.println(app.name+"이 켜졌습니다.");

75051788ef6a96b8a40c817b7b6c2b7e29a183ce
kbeomjun <98301971+kbeomjun@users.noreply.github.com>
2024-06-16 10:24:05 +0900

    자바 13일차 - 과제 Phone

--- java_kbj/src/day13/homework/PhoneEx01.java
+++ java_kbj/src/day13/homework/PhoneEx01.java
@@ -9,7 +9,7 @@ public class PhoneEx01 {
 	public static void main(String[] args) {
 		/* 연락처 관리를 위한 프로그램 
 		 * 번호가 같으면 등록이 불가 */
-		int menu = 0, phoneCount = 0;
+		int menu = 0, phoneCount = 0, count = 0;
 		Phone[] list = new Phone[2];
 		
 		do {
@@ -26,31 +26,41 @@ public class PhoneEx01 {
 				}
 				break;
 			case 2:
-				System.out.print("이름 입력 : ");
-				String name = scan.next();
-				for(int i = 0; i < phoneCount; i++) {
-					if(list[i].getName().equals(name)) {
-						System.out.print(i+". ");
-						list[i].print();
-					}
+				count = findNum(list, phoneCount, count);
+				if(count == 0) {
+					System.out.println("없는 번호입니다.");
+					break;
 				}
-				System.out.print("번호 선택 : ");
-				int num = scan.nextInt();
-				
+				phoneCount = modifyNum(list, phoneCount);
+				for(int i = 0; i < phoneCount; i++) {
+					list[i].print();
+				}
 				break;
 			case 3:
+				count = findNum(list, phoneCount, count);
+				if(count == 0) {
+					System.out.println("없는 번호입니다.");
+					break;
+				}
+				list = deletePhone(list, phoneCount--);
+				for(int i = 0; i < phoneCount; i++) {
+					list[i].print();
+				}
 				break;
 			case 4:
+				count = findNum(list, phoneCount, count);
+				if(count == 0) {
+					System.out.println("없는 번호입니다.");
+					break;
+				}
 				break;
 			case 5:
 				System.out.println("프로그램을 종료합니다.");
 				break;
 			default:
-				
+				System.out.println("잘못된 메뉴입니다.");
 			}
 		}while(menu != 5);
-		
-		
 	}
 	
 	public static void printMenu() {
@@ -66,7 +76,7 @@ public class PhoneEx01 {
 	public static Phone inputNum() {
 		System.out.print("이름 : ");
 		String name = scan.next();
-		System.out.print("번호 : ");
+		System.out.print("번호(예 : xxx-xxxx-xxxx) : ");
 		String num = scan.next();
 		return new Phone(name, num);
 	}
@@ -94,9 +104,51 @@ public class PhoneEx01 {
 		System.out.println("-------------------------");
 		return phoneCount;
 	}
-
 	
-
+	public static int findNum(Phone[] list, int phoneCount, int count) {
+		System.out.print("이름 입력 : ");
+		String name = scan.next();
+		count = 0;
+		for(int i = 0; i < phoneCount; i++) {
+			if(list[i].getName().contains(name)) {
+				System.out.print((i+1)+". ");
+				list[i].print();
+				count++;
+			}
+		}
+		return count;
+	}
+	
+	public static int modifyNum(Phone[] list, int phoneCount) {
+		System.out.print("수정할 번호 선택 : ");
+		int num = scan.nextInt();
+		Phone tmp = inputNum();
+		for(int i = 0; i < phoneCount; i++) {
+			if(i == num - 1) {
+				continue;
+			}
+			if(tmp.getNum().equals(list[i].getNum())) {
+				System.out.println("이미 등록된 번호입니다.");
+				return phoneCount;
+			}
+		}
+		list[num - 1] = tmp;
+		System.out.println("번호 수정을 완료했습니다.");
+		System.out.println("-------------------------");
+		return phoneCount;
+	}
+	
+	public static Phone[] deletePhone(Phone[] list, int phoneCount) {
+		System.out.print("삭제할 번호 선택 : ");
+		int num = scan.nextInt();
+		Phone[] tmp = new Phone[phoneCount - 1];
+		for(int i = num - 1; i < phoneCount - 1; i++) {
+			tmp[i] = list[i + 1];
+		}
+		System.out.println("번호 삭제를 완료했습니다.");
+		System.out.println("-------------------------");
+		return tmp;
+	}
 }
 
 class Phone{
@@ -110,7 +162,7 @@ class Phone{
 	}
 	
 	public void print() {
-		System.out.println("이름 : "+name+" 번호 : "+num);
+		System.out.println("이름 : "+name+" | 번호 : "+num);
 	}
 
 	public String getName() {return name;}

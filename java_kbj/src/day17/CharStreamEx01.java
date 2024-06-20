3098f04236b11bb4749b3a73a95f5f83bd9519fb
kbeomjun <98301971+kbeomjun@users.noreply.github.com>
2024-06-20 17:13:29 +0900

    자바 17일차 - CharStream

---
+++ java_kbj/src/day17/CharStreamEx01.java
@@ -0,0 +1,31 @@
+package day17;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CharStreamEx01 {
+
+	public static void main(String[] args) {
+		String fileName = "src/day17/byteStream.txt";
+		try(FileReader fr = new FileReader(fileName)){
+			while(fr.ready()) {
+				char ch = (char)fr.read();
+				System.out.print(ch);
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("파일을 찾을 수 없습니다.");
+		} catch (IOException e) {
+			System.out.println("파일 입출력에서 예외가 발생했습니다.");
+		}
+		
+		String fileName2 = "src/day17/charStream.txt";
+		try(FileWriter fw = new FileWriter(fileName2)){
+			String str = "안녕하세요. 제 이름은 홍길동입니다.\n만나서 반갑습니다.";
+			fw.write(str);
+		} catch (IOException e) {
+			System.out.println("파일 입출력 예외가 발생했습니다.");
+		}
+	}
+}

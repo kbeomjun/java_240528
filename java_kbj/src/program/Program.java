b4adab1e97647902352abb0ff40af95449f685e4
kbeomjun <98301971+kbeomjun@users.noreply.github.com>
2024-08-07 14:10:04 +0900

    자바 - Program

--- java_kbj/src/program/Program.java
+++ java_kbj/src/program/Program.java
@@ -1,15 +1,9 @@
 package program;
 
 public interface Program {
-	
 	void printMenu();
-	
 	void runMenu(int menu) throws Exception;
-
 	void run();
-	
 	void save(String fileName);
-	
 	void load(String fileName);
-	
 }

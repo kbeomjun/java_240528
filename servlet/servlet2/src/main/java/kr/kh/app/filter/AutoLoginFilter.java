c06493682f71edfcbc70e951bd0d22c680585c24
kbeomjun <98301971+kbeomjun@users.noreply.github.com>
2024-08-27 15:49:00 +0900

    자동로그인 필터 수정

--- servlet/servlet2/src/main/java/kr/kh/app/filter/AutoLoginFilter.java
+++ servlet/servlet2/src/main/java/kr/kh/app/filter/AutoLoginFilter.java
@@ -1,6 +1,7 @@
 package kr.kh.app.filter;
 
 import java.io.IOException;
+import java.util.Date;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -23,7 +24,6 @@ public class AutoLoginFilter extends HttpFilter implements Filter {
 	private MemberService memberService = new MemberServiceImp();
     
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		
 		//세션에 저장된 회원 정보를 가져옴
 		HttpServletRequest hrequest = (HttpServletRequest)request;
 		HttpSession session = hrequest.getSession();
@@ -35,7 +35,6 @@ public class AutoLoginFilter extends HttpFilter implements Filter {
 		}
 		
 		//로그인이 안됐을 때(null일때)
-		
 		//쿠키 정보를 가져옴(AL)
 		Cookie cookie = getCookie(hrequest, "AL");
 		
@@ -45,18 +44,17 @@ public class AutoLoginFilter extends HttpFilter implements Filter {
 		}
 		
 		//쿠키 정보가 있으면 
-	
 		//쿠키에 있는 세션값을 가져옴
 		String sid = cookie.getValue();
 		//서비스에게 세션값을 주면서 일치하는 회원 정보를 달라고 요청
 		user = memberService.getMemberBySid(sid);
 		//회원 정보를 가져오면
-		if(user != null) {
+		Date date = new Date(System.currentTimeMillis());
+		if(user != null && user.getMe_limit().compareTo(date) > 0) {
 			//세션에 저장
 			session.setAttribute("user", user);
 		}
 		chain.doFilter(request, response);
-		
 	}
 
 	private Cookie getCookie(HttpServletRequest hrequest, String name) {

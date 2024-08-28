1e3330a6792fe661ec0347dcc3086254c9c81317
kbeomjun <98301971+kbeomjun@users.noreply.github.com>
2024-08-28 16:48:02 +0900

    커뮤니티, 게시글 목록, 게시글 등록

--- spring projects/spring/src/main/java/kr/kh/spring/dao/PostDAO.java
+++ spring projects/spring/src/main/java/kr/kh/spring/dao/PostDAO.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.annotations.Param;
 
 import kr.kh.spring.Pagination.PostCriteria;
 import kr.kh.spring.model.vo.CommunityVO;
+import kr.kh.spring.model.vo.FileVO;
 import kr.kh.spring.model.vo.PostVO;
 
 public interface PostDAO {
@@ -14,4 +15,8 @@ public interface PostDAO {
 	List<PostVO> selectPostList(@Param("cri")PostCriteria cri);
 
 	int selectPostTotalCount(@Param("cri")PostCriteria cri);
+
+	boolean insertPost(@Param("po")PostVO post);
+
+	void insertFile(@Param("fi")FileVO fileVO);
 }
--- spring projects/spring/src/main/java/kr/kh/spring/service/PostService.java
+++ spring projects/spring/src/main/java/kr/kh/spring/service/PostService.java
@@ -2,19 +2,27 @@ package kr.kh.spring.service;
 
 import java.util.List;
 
+import javax.annotation.Resource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import kr.kh.spring.Pagination.PageMaker;
 import kr.kh.spring.Pagination.PostCriteria;
 import kr.kh.spring.dao.PostDAO;
 import kr.kh.spring.model.vo.CommunityVO;
+import kr.kh.spring.model.vo.FileVO;
+import kr.kh.spring.model.vo.MemberVO;
 import kr.kh.spring.model.vo.PostVO;
+import kr.kh.spring.utils.UploadFileUtils;
 
 @Service
 public class PostService {
 	@Autowired
 	PostDAO postDao;
+	@Resource
+	String uploadPath;
 
 	public List<CommunityVO> getCommunityList() {
 		return postDao.selectCommunityList();
@@ -33,5 +41,46 @@ public class PostService {
 		}
 		int totalCount = postDao.selectPostTotalCount(cri);
 		return new PageMaker(2, cri, totalCount);
+	}
+
+	public boolean insertPost(PostVO post, MemberVO user, MultipartFile[] fileList) {
+		if(post == null || user == null) {
+			return false;
+		}
+		
+		boolean res;
+		try {
+			post.setPo_me_id(user.getMe_id());
+			res = postDao.insertPost(post);
+		}catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		if(!res) {
+			return false;
+		}
+		
+		if(fileList == null || fileList.length == 0) {
+			return true;
+		}
+		for(MultipartFile file : fileList) {
+			uploadFile(file, post.getPo_num());
+		}
+		return true;
+	}
+
+	private void uploadFile(MultipartFile file, int po_num) {
+		if(file == null || file.getOriginalFilename().length() == 0) {
+			return;
+		}
+		try {
+			String fi_ori_name = file.getOriginalFilename();
+			// 첨부파일을 서버에 업로드 후 경로가 포함된 파일명을 가져옴
+			String fi_name = UploadFileUtils.uploadFile(uploadPath, fi_ori_name, file.getBytes());
+			FileVO fileVO = new FileVO(fi_name, fi_ori_name, po_num);
+			postDao.insertFile(fileVO);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }

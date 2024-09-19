176dec90a17bb54af7ccf79303dd27cf23786b09
kbeomjun <98301971+kbeomjun@users.noreply.github.com>
2024-09-19 12:22:42 +0900

    리액트 5일차 - 스프링 연동(데이터 전송)

--- react/react1/src/index.js
+++ react/react1/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import App from './App';
-import RouteApp from './route/RouteApp';
+// import RouteApp from './route/RouteApp';
+import UseEffectEx from './useEffectEx/UseEffectEx';
+import FetchEx1 from './fetchEx/FetchEx1';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     // <App />
-    <RouteApp />
+    // <RouteApp />
+    // <UseEffectEx />
+    <FetchEx1 />
 );
---
+++ spring projects/spring3/src/main/java/kr/kh/spring3/model/dto/PersonDTO.java
@@ -0,0 +1,13 @@
+package kr.kh.spring3.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PersonDTO {
+	String name;
+	int age;
+}
